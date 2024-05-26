5bcca5351086bc4a2d38dddb2c7cc5170bd1f39c
“[Breno <manancialgd@gmail.com>
2024-05-26 21:50:50 +0100

    Balance

--- YetAnotherDungeonCrawler/Map.cs
+++ YetAnotherDungeonCrawler/Map.cs
@@ -19,7 +19,8 @@ namespace YetAnotherDungeonCrawler.Models
             // Room 2: A zombie with 10 attack power and 50 HP & An potion Level 2
             ItemBase[] itemsRoom2 = new ItemBase[]
             {
-                new HealPotion(2)
+                new WoodenSword(1)
+
             };
             Enemy enemyRoom2 = new Zombie(10, 30);
             Rooms.Add(new Room(2, itemsRoom2, enemyRoom2));
@@ -27,7 +28,7 @@ namespace YetAnotherDungeonCrawler.Models
             // Room 3: A most powerfull zombie with 30 attack damage and 50 hp & a WoodenSword
             ItemBase[] itemsRoom3 = new ItemBase[]
             {
-                new WoodenSword(1)
+                new HealPotion(2)
             };
             Enemy enemyRoom3 = new Zombie(30, 40);
             Rooms.Add(new Room(3, itemsRoom3, enemyRoom3));
@@ -37,7 +38,7 @@ namespace YetAnotherDungeonCrawler.Models
             {
 
             };
-            Enemy enemyRoom4 = new Zombie(50, 50);
+            Enemy enemyRoom4 = new Zombie(30, 70);
             Rooms.Add(new Room(4, itemRoom4, enemyRoom4));
         }
     }
